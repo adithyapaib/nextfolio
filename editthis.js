11a2a52b1e7b73ce2ef07a8fcc79e5391dee9612
Adithya Pai B <paiadithya26@gmail.com>
2020-12-07 10:16:02 +0530

    added editthis.js for easy changes

---
+++ editthis.js
@@ -0,0 +1,5 @@
+var site = {
+    name : 'Adithya Pai B',
+    pro: 'Front-End Developer'
+}
+export default site;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-
+import user from '../editthis';
+import HEAD from 'next/head';
 export default function Hello()
 {
   return <>
+  <HEAD>
+<title>{user.name} | {user.pro}</title>
+<link rel="shortcut icon" type="image/png" href="https://img.icons8.com/windows/32/000000/code.png"/>
+  </HEAD>
   <div className='main'> 
-   <h1 className='me'>Adithya Pai</h1>
+<h1 className='me'>{user.name}</h1>
    <h2 className='job'>Front-End Developer</h2>
   </div>
   </>
