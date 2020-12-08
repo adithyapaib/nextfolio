b7a4bec40010fe65a6376dbebee3af347231cbef
Adithya Pai B <paiadithya26@gmail.com>
2020-12-08 20:45:11 +0530

    foe

--- editthis.js
+++ editthis.js
@@ -1,8 +1,8 @@
 var site = {
     name : 'Adithya Pai B',
     job: 'Front-End Developer',
-    decription: `Hi there! I am Adithya Pai B from Mangalore, India-flag
+    decription: `Hi there! I am Adithya Pai B from Mangalore, India.
     I am currently pursuing Bachelor of Engineering in Computer Science.
-    I heart icon to design Websites, UI/UX, and Photography `
+    I ❤️ to design Websites, UI/UX, and Photography `
 }
 export default site;
