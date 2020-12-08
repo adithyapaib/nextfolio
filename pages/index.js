d36dd5b6f264e92e0a21ab71ade826beeccdbd67
Adithya Pai B <paiadithya26@gmail.com>
2020-12-08 12:39:18 +0530

    about text added

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,12 @@ maximum-scale=1.0, user-scalable=no" />
    <img  src='/img/moon.svg' className='moon'></img>
   </div>
   <div className="about">
-    lorem
+  <h1>
+    About Me
+  </h1>
+  <p>
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  </p>
 
   </div>
   
