49fd294f2c12d3bcf3e44570f76b27bfe1e750c5
Adithya Pai B <paiadithya26@gmail.com>
2020-12-08 20:21:14 +0530

    scroll snap addedd

--- editthis.js
+++ editthis.js
@@ -1,5 +1,8 @@
 var site = {
     name : 'Adithya Pai B',
-    job: 'Front-End Developer'
+    job: 'Front-End Developer',
+    decription: `Hi there! I am Adithya Pai B from Mangalore, India-flag
+    I am currently pursuing Bachelor of Engineering in Computer Science.
+    I heart icon to design Websites, UI/UX, and Photography `
 }
 export default site;
--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,16 @@ maximum-scale=1.0, user-scalable=no" />
    <img  src='/img/moon.svg' className='moon'></img>
   </div>
   <div className="about">
-  <h1>
-    About Me
-  </h1>
-  <p>
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  </p>
+  <h1>About Me</h1>
+  <div className='img'>
+  <img src="img/ill.svg"/>
+  </div>
+
+  <br/>
+<p>{user.decription}</p>
 
   </div>
+  
   <div className='skills'>
     hello
 
