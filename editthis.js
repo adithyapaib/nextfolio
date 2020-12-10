1b89d928cb39334283595deca09ed3133bd450a5
Adithya Pai B <paiadithya26@gmail.com>
2020-12-10 22:01:04 +0530

    added connect pages

--- editthis.js
+++ editthis.js
@@ -4,7 +4,8 @@ var site = {
     description: `Hi there! I am Adithya Pai B from Mangalore, India.
     I am currently pursuing Bachelor of Engineering in Computer Science.
     I ❤️ to design Websites, UI/UX, and Photography `,
-    skills: [['React', 'JS', 'HTML', 'CSS','Javascript'], ['PS', 'XD']]
+    skills_title: ['Web Development', 'Design'],
+    skills: [['HTML', 'CSS', 'JS', 'ReactJS','SCSS'], ['Photoshop', 'XD',]]
    
     
 }
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,44 @@
 import user from '../editthis';
 import HEAD from 'next/head';
-const listItems = user.skills[0].map((number) =><li>{number}</li>);
-const listItems2 = user.skills[1].map((number) =><li>{number}</li>);
+const listItems = user.skills[0].map((number) => <li>{number}</li>);
+const listItems2 = user.skills[1].map((number) => <li>{number}</li>);
 export default function Hello() {
   return <>
     <HEAD>
       <title>{user.name} | {user.job}</title>
       <meta name="description" content={user.description}></meta>
-      <meta name="theme-color" content="#ff0057"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta name="theme-color" content="#ff0057" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="shortcut icon" type="image/png" href="https://img.icons8.com/windows/32/000000/code.png" />
     </HEAD>
     <div className='main'>
       <div className='wrapper'>
         <div className='landing'>
-        <section className='section'></section>
+          <section className='section'></section>
           <h1 className='name'>{user.name}</h1>
           <h2 className='job'>{user.job}</h2>
           <img src='/img/coding.svg' className='moon' alt='coding svg illustration'></img>
         </div>
         <div className="about">
-        <section className='section'></section>
+          <section className='section'></section>
           <h1>About Me</h1>
           <div className='img'>
             <img src="img/goals.svg" alt='Boy illustration' />
           </div>
-
           <br />
           <p>{user.description}</p>
-
         </div>
-
         <div className='skills'>
           <h1>Skills</h1>
-          <div className='skill'>
-            <ul>{listItems}</ul>
-            <ul>{listItems2}</ul>
-          </div>
+          <h2>{user.skills_title[0]}</h2>
+          <ul>{listItems}</ul>
+          <h2>{user.skills_title[1]}</h2>
+          <ul>{listItems2}</ul>
+          <section className='section'></section>
+        </div>
+        <div className='connect'> 
+         <h1>Social Media</h1>
+
         </div>
 
       </div>
