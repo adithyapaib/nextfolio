e242fedee5749e71b2c3201b59c3b98cdacf57da
Adithya Pai B <paiadithya26@gmail.com>
2020-12-11 10:45:55 +0530

    skill page update

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,8 @@ export default function Hello() {
           <ul>{listItems}</ul>
           <h2>{user.skills_title[1]}</h2>
           <ul>{listItems2}</ul>
-          <section className='section'></section>
+          {/* <section className='section'></section> */}
+          <div className='img'><img  src='img/patternpad.svg'></img> </div>
         </div>
         <div className='connect'> 
          <h1>Social Media</h1>
