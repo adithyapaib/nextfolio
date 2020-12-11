058b763d09297e7ac8505fcffdb5bba507bea4db
Adithya Pai B <paiadithya26@gmail.com>
2020-12-11 21:30:40 +0530

    performance update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import user from '../editthis';
 import HEAD from 'next/head';
+import Aos from 'aos';
+import "aos/dist/aos.css";
 const listItems = user.skills[0].map((number) => <li>{number}</li>);
 const listItems2 = user.skills[1].map((number) => <li>{number}</li>);
 const listItems3 = user.skills[2].map((number) => <li>{number}</li>);
@@ -47,25 +49,22 @@ export default function Hello() {
         <div className='connect'> 
          <h1>Connect</h1>
          <ul>
-           <li> <a href={user.social.github} target='_blank'><i class="fab fa-github"></i> </a></li>
-           <li> <a href={user.social.linkedin} target='_blank'><i class="fab fa-linkedin"></i></a></li>
-           <li> <a href={user.social.twitter} target='_blank'><i class="fab fa-twitter"></i> </a></li>
-           <li> <a href={user.social.mail} target='_blank'><i class="far fa-envelope"></i></a></li>
-           <li> <a href={user.social.instagram} target='_blank'><i class="fab fa-instagram"></i></a></li>
+           <li> <a href={user.social.github} target='_blank' rel="noreferrer"><i class="fab fa-github"></i> </a></li>
+           <li> <a href={user.social.linkedin} target='_blank' rel="noreferrer"><i class="fab fa-linkedin"></i></a></li>
+           <li> <a href={user.social.twitter} target='_blank'rel="noreferrer" ><i class="fab fa-twitter"></i> </a></li>
+           <li> <a href={user.social.mail} target='_blank' rel="noreferrer"><i class="far fa-envelope"></i></a></li>
+           <li> <a href={user.social.instagram} target='_blank' rel="noreferrer"><i class="fab fa-instagram"></i></a></li>
 
          </ul>
          <h2>Contact Me</h2>
         <form action={user.form} method="POST" className='form' >
-     <input type="text" name="name" required placeholder='Name'/>
-     <input type="email" name="email" required placeholder='Email' />
-     <textarea placeholder='Message'></textarea>
+        <label for="name"></label> <input id='name' name="name" type="text" name="name" required placeholder='Name'/>
+        <label for="email"></label><input type="email" id="email" name="email"required placeholder='Email' />
+        <label for="message"></label><textarea name='message' placeholder='Message'></textarea>
      <button type="submit">Send</button>
 </form>
-
         </div>
-
       </div>
     </div>
-
   </>
 }
