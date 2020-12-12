f26841c2a8888d738f6a7849183e3f374c1df6e0
Adithya Pai B <paiadithya26@gmail.com>
2020-12-12 20:14:04 +0530

    final changes and documentaiton

--- editthis.js
+++ editthis.js
@@ -1,12 +1,15 @@
 var site = {
     name : 'Adithya Pai B',
     job: 'Front-End Developer',
+       /* This decription is used both in meta tag and about page */
     description: `Hi there! I am Adithya Pai B from Mangalore, India.
     I am currently pursuing Bachelor of Engineering in Computer Science.
     I ❤️ to design Websites, UI/UX, and Photography.`,
+    /* Change your skills and title accodingly */
     skills_title: ['Web Development', 'Design','Programming'],
     skills: [['HTML', 'CSS', 'JS', 'ReactJS','SCSS'], ['Photoshop', 'XD','Figma'],['C','C++','Python3', 'Javascript']],
     social: {
+        /* Add your social media links here */
         github: 'https://github.com/adithyapaib',
         twitter: 'https://twitter.com/adithyapaib',
         linkedin: 'https://www.linkedin.com/in/adithyapaib/',
--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Hello() {
           <section className='section'></section>
           <h1 className='name'>{user.name}</h1>
           <h2 className='job'>{user.job}</h2>
-          <br/>
-         {/*  <h4 className='lm'><a href='#about'>Scroll Right !</a></h4> */}
+          <br />
+          {/*  <h4 className='lm'><a href='#about'>Scroll Right !</a></h4> */}
           <img src='/img/coding.svg' className='moon' alt='coding svg illustration'></img>
         </div>
         <div className="about" id='about'>
@@ -43,37 +43,37 @@ export default function Hello() {
           <ul>{listItems}</ul>
           <h2>{user.skills_title[1]}</h2>
           <ul>{listItems2}</ul>
-           <h2>{user.skills_title[2]}</h2>
-           <ul>{listItems3}</ul>
+          <h2>{user.skills_title[2]}</h2>
+          <ul>{listItems3}</ul>
           {/* <section className='section'></section> */}
-          <div className='img'><img  src='img/patternpad.svg' alt='pattern'></img> </div>
+          <div className='img'><img src='img/patternpad.svg' alt='pattern'></img> </div>
         </div>
-        <div className='connect'> 
-         <h1>Connect</h1>
-         <ul>
-           <li> <a href={user.social.github} target='_blank' rel="noreferrer"><i class="fab fa-github"></i> </a></li>
-           <li> <a href={user.social.linkedin} target='_blank' rel="noreferrer"><i class="fab fa-linkedin"></i></a></li>
-           <li> <a href={user.social.twitter} target='_blank'rel="noreferrer" ><i class="fab fa-twitter"></i> </a></li>
-           <li> <a href={user.social.mail} target='_blank' rel="noreferrer"><i class="far fa-envelope"></i></a></li>
-           <li> <a href={user.social.instagram} target='_blank' rel="noreferrer"><i class="fab fa-instagram"></i></a></li>
+        <div className='connect'>
+          <h1>Connect</h1>
+          <ul>
+            <li> <a href={user.social.github} target='_blank' rel="noreferrer"><i class="fab fa-github"></i> </a></li>
+            <li> <a href={user.social.linkedin} target='_blank' rel="noreferrer"><i class="fab fa-linkedin"></i></a></li>
+            <li> <a href={user.social.twitter} target='_blank' rel="noreferrer" ><i class="fab fa-twitter"></i> </a></li>
+            <li> <a href={user.social.mail} target='_blank' rel="noreferrer"><i class="far fa-envelope"></i></a></li>
+            <li> <a href={user.social.instagram} target='_blank' rel="noreferrer"><i class="fab fa-instagram"></i></a></li>
 
-         </ul>
-         <h2>Contact Me</h2>
-        <form action={user.form} method="POST" className='form' >
-     {/*    <label for="name"></label> <input id='name' name="name" type="text" name="name" required placeholder='Name'/>
+          </ul>
+          <h2>Contact Me</h2>
+          <form action={user.form} method="POST" className='form' >
+            {/*    <label for="name"></label> <input id='name' name="name" type="text" name="name" required placeholder='Name'/>
         <label for="email"></label><input type="email" id="email" name="email"required placeholder='Email' />
         <label for="message"></label><textarea name='message' placeholder='Message'></textarea> */}
-  <label for="name">Name</label> 
- <input type="text" id="name" name="name" required/>
- <label for="email">Email</label> 
- <input type="email" id="email" name="email" required/>
- <label for="message">Message </label>
- <textarea class="form-control" id="message" name="message" rows="1" required></textarea>
-     <button type="submit">Send</button>
-</form>
-<div className='img-social'>
-<img src='/img/social.svg' className='img-social' alt='Connect Illustration'></img>
-</div>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" required />
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required />
+            <label for="message">Message </label>
+            <textarea class="form-control" id="message" name="message" rows="1" required></textarea>
+            <button type="submit">Send</button>
+          </form>
+          <div className='img-social'>
+            <img src='/img/social.svg' className='img-social' alt='Connect Illustration'></img>
+          </div>
 
         </div>
       </div>
