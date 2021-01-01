17d75ddc7e3e7c45321a4a5a2091fb2c62b2145b
Adithya Pai B <paiadithya26@gmail.com>
2021-01-01 10:54:08 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Hello() {
             <label for="name">Name</label>
             <input type="text" id="name" name="name" required />
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" required />
+            <input type="email" id="email" name="email" inputmode="email" required />
             <label for="message">Message </label>
             <textarea class="form-control" id="message" name="message" rows="1" required></textarea>
             <button type="submit">Send</button>
