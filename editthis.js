a403fb34501cfd0fdc406c38c0e464afeff0a22e
TANISHA SINGHAL <88341606+Tanisha01-small@users.noreply.github.com>
2023-02-13 17:45:51 +0530

    Profile

--- editthis.js
+++ editthis.js
@@ -1,20 +1,20 @@
 var site = {
       /* Your name */
-    name : 'Adithya Pai',
-    job: 'Front-End Developer',
+    name : 'Tanisha Singhal',
+    job: 'Front-End Developer and Business Analyst',
        /* This decription is used both in meta tag and about page */
     description: `Hi there! I am Adithya Pai B from Mangalore, India.
     I am currently pursuing Bachelor of Engineering in Computer Science.
     I ❤️ to design Websites, UI/UX, and Photography.`,
     /* Change your skills and title accodingly */
-    skills_title: ['Web Development', 'Design','Programming'],
-    skills: [['HTML', 'CSS', 'JS', 'ReactJS','SCSS'], ['Photoshop', 'XD','Figma'],['C','C++','Python3', 'Javascript']],
+    skills_title: ['Web Development','Programming'],
+    skills: [['HTML', 'CSS', 'JS', 'ReactJS','MYSQL'], ['Photoshop', 'XD','Figma'],['JAVA','Python3','Javascript']],
     skillp:[[100, 80 ,100, 60, 80], [50, 60, 50], [70,80,90,90]],
     social: {
         /* Add your social media links here */
-        github: 'https://github.com/adithyapaib',
+        github: 'https://github.com/Tanisha01-small',
         twitter: 'https://twitter.com/adithyapaib',
-        linkedin: 'https://www.linkedin.com/in/adithyapaib/',
+        linkedin: 'http://www.linkedin.com/in/tanisha01singhal',
         mail: 'mailto: email@gmail.com',
         instagram: 'https://instagram.com'
     },
